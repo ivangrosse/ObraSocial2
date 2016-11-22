514c3fc88b08e77b7b4b16800799f9475381fef7
martin <martin@DESKTOP-C1KHOL2>
2016-11-22 06:04:17 -0300

    Nueva rama - Proyecto web 2

---
+++ ObraSocial/ObraSocialWeb/App_Start/RouteConfig.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace ObraSocialWeb
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("");
+
+            routes.MapRoute(
+                "Default",
+                "{controller}/{action}/{id}",
+                new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
+
