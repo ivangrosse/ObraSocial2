e91f49676311c1ea025afcfae283196ba399eb05
Ivan <ivangrosso@gmail.com>
2016-11-23 02:02:59 -0300

    Valdiar afiliado por web service funcionando, en el proyecto web 2

---
+++ ObraSocial/ObraSocialWeb_2/ObraSocialWebService.asmx.cs
@@ -0,0 +1,34 @@
+﻿using ObraSocialNegocio.Modelo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace ObraSocialWeb_2
+{
+    /// <summary>
+    /// Summary description for ObraSocialWebService
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class ObraSocialWebService : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public string validarAfiliado(int nroAfiliado)
+        {
+
+            Afiliado afiliado = new Afiliado();
+            afiliado = ObraSocialNegocio.ADO.AfiliadoADO.buscarAfiliado(nroAfiliado);
+            if (afiliado == null)
+            {
+                return "No se encontro el afiliado";
+            }
+            return afiliado.Nombre + " " + afiliado.Apellido + " es afiliado";
+        }
+    }
+}
