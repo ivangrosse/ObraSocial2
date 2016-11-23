98c6595fc6d08df3b0a218badda0ffe4def01cca
Ivan <ivangrosso@gmail.com>
2016-11-22 22:27:57 -0300

    Referencia de Negocio en Desktop

---
+++ ObraSocial/ObraSocialDesktop/Controlador/ControladorAfiliado.cs
@@ -0,0 +1,20 @@
+﻿using ObraSocialNegocio.Modelo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ObraSocialDesktop.Controlador
+{
+    public class ControladorAfiliado
+    {
+
+
+        public void agregarAfiliado(Afiliado a)
+        {
+
+        }
+    }
+}
