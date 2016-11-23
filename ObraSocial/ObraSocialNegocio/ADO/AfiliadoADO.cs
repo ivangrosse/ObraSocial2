c1ae255ee69b0501c6830818bb6276500f68dc18
Ivan <ivangrosso@gmail.com>
2016-11-23 00:09:34 -0300

    AfiliadoADO metodo INSERT

--- ObraSocial/ObraSocialNegocio/ADO/AfiliadoADO.cs
+++ ObraSocial/ObraSocialNegocio/ADO/AfiliadoADO.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using ObraSocialNegocio.Modelo;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,26 @@ namespace ObraSocialNegocio.ADO
 {
     public class AfiliadoADO
     {
+        public void altaAfiliado(Afiliado afiliado) { 
+        String connectionString = Configuracion.STRING_DE_CONEXION_IVAN;
+        String result = "UNSuccessful";
+        SqlConnection conn = new SqlConnection(connectionString);
+            try
+            {
+                conn.Open();
+                String query = "INSERT INTO AFILIADO VALUES(" + "'" + afiliado.Dni + "', '" + afiliado.Nombre + "', '" + afiliado.Apellido + "')";
+                SqlCommand cmd = new SqlCommand(query, conn);
+
+                cmd.ExecuteNonQuery();
+
+
+            }
+            catch (Exception e)
+            {
+                result = e.Message;
+            }
+            conn.Close();
+
+        }
     }
 }
