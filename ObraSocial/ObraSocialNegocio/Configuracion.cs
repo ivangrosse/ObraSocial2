60fa4f292425c0cfefd882a15256421604dee1ad
Ivan <ivangrosso@gmail.com>
2016-11-22 22:31:52 -0300

    String conexion

--- ObraSocial/ObraSocialNegocio/Configuracion.cs
+++ ObraSocial/ObraSocialNegocio/Configuracion.cs
@@ -8,6 +8,6 @@ namespace ObraSocialNegocio
 {
     class Configuracion
     {
-        public static String STRING_DE_CONEXION = "";
+        public static String STRING_DE_CONEXION = "Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Ivan\mingw\Documents\ObraSocial.mdf;Integrated Security=True;Connect Timeout=30";
     }
 }
