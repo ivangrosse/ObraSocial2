502e9c33e72479361552f6cbb6eb210e08447e13
martin <martin@DESKTOP-C1KHOL2>
2016-11-23 01:35:22 -0300

    Agrego cadena de conexion

--- ObraSocial/ObraSocialNegocio/Configuracion.cs
+++ ObraSocial/ObraSocialNegocio/Configuracion.cs
@@ -8,6 +8,7 @@ namespace ObraSocialNegocio
 {
     class Configuracion
     {
-        public static String STRING_DE_CONEXION = "Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Ivan\mingw\Documents\ObraSocial.mdf;Integrated Security=True;Connect Timeout=30";
+        public static String STRING_DE_CONEXION_IVAN = "Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Ivan\mingw\Documents\ObraSocial.mdf;Integrated Security=True;Connect Timeout=30";
+        public static String STRING_DE_CONEXION_MARTIN = "Data Source=(LocalDB)/MSSQLLocalDB;AttachDbFilename=C:/Users/martin/Documents/ObraSocial.mdf;Integrated Security=True;Connect Timeout=30";
     }
 }
---
+++ ObraSocial/ObraSocialWeb/Controllers/MedicController.cs
@@ -0,0 +1,16 @@
+﻿using ObraSocialNegocio.Modelo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ObraSocialWeb.Controllers
+{
+    public class MedicController
+    {
+        public void AltaMedico(Medico medico)
+        {
+            
+        }
+    }
+}
