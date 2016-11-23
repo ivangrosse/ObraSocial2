8f10f10379954c300217d262ddac30e0e37437d2
martin <martin@DESKTOP-C1KHOL2>
2016-11-23 01:40:24 -0300

    Configuracion.cs martin

--- ObraSocial/ObraSocialNegocio/Configuracion.cs
+++ ObraSocial/ObraSocialNegocio/Configuracion.cs
@@ -8,6 +8,6 @@ namespace ObraSocialNegocio
 {
     class Configuracion
     {
-        public static String STRING_DE_CONEXION = "Data Source=(LocalDB)/MSSQLLocalDB;AttachDbFilename=C:/Users/Ivan/mingw/Documents/ObraSocial.mdf;Integrated Security=True;Connect Timeout=30";
+        public static String STRING_DE_CONEXION_IVAN = "Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Ivan\mingw\Documents\ObraSocial.mdf;Integrated Security=True;Connect Timeout=30";
     }
 }
